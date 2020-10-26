72475831abe023d2a1f43ec915ba241d3b25f6d3
Jeremy Kun <j2kun@users.noreply.github.com>
2020-10-26 10:26:57 -0700

    extract model-agnostic parts to model.py

--- kernel_density.py
+++ kernel_density.py
@@ -5,67 +5,39 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import pandas as pd
 import seaborn as sns
+from model import Model
+from model import ExperimentParameters
+from model import run_experiment
 
 
-def learn_model(data):
+def fit_kde(data):
     snp_percent_change = data["Percent change"].to_numpy()
     kde = gaussian_kde(snp_percent_change)
-    return lambda n: kde.resample(size=n)[0]
-
-
-def run_experiment(sampler, years=15, capital=400000):
-    num_months = years * 12
-    starting_capital = capital
-    samples = sampler(num_months)
-
-    for monthly_pct_change in samples:
-        capital *= (1 + monthly_pct_change / 100)
-        if capital <= 0:
-            return starting_capital
-
-    return capital - starting_capital
-
-
-def run_experiments(sampler, years=15, capital=400000, num_runs=1000):
-    results = []
-    for i in range(num_runs):
-        results.append(run_experiment(sampler, years=years, capital=capital))
-    return results
-
-
-def compute_loan_interest(loan_amt, yearly_rate, years=15):
-    return loan_amt * (1 + yearly_rate / 12)**(12 * years) - loan_amt
+    return Model(name="Kernel Density Estimator",
+                 sampler=lambda n: list(kde.resample(size=n)[0]))
 
 
 if __name__ == "__main__":
     data = pd.read_csv("snp.tsv", sep='\t')
-    sampler = learn_model(data.head(50*12))
+    model = fit_kde(data.head(50 * 12))
 
-    house_price = 1000000
-    cash_on_hand = 700000
-    mortgage_rate = 0.03
-    down_payments = [200000 + i * 50000 for i in range(10)]
-    loan_interests = dict(
-        (dp, compute_loan_interest(house_price - dp, mortgage_rate))
-        for dp in down_payments)
-    capital_values = dict((dp, cash_on_hand-dp) for dp in down_payments)
+    params = ExperimentParameters(
+        years=30,
+        house_price_usd=1000000,
+        on_hand_usd=700000,
+        mortgage_rate=0.03,
+    )
 
-    results_dict = dict()
-    for dp, capital in capital_values.items():
-        print("Simulating for $%.2f starting capital" % capital)
-        results = run_experiments(sampler, capital=capital)
-        profits_minus_loan_interest = [
-                profit - loan_interests[dp] for profit in results]
-        results_dict["{:d}k".format(dp // 1000)] = profits_minus_loan_interest
-
-    df = pd.DataFrame(data=results_dict)
+    results = run_experiment(model, params)
+    df = pd.DataFrame(data=results)
 
     fig, axes = plt.subplots()
     sns.violinplot(data=df, ax=axes, scale="width")
     # axes = df.boxplot()
     axes.set_title("Larger down payment vs Investing in S&P 500")
-    axes.set_xlabel("Down payment (700k - x is invested)")
-    axes.set_ylabel("15 year profit minus loan interest")
+    axes.set_xlabel(
+        f"Down payment ({params.on_hand_usd / 1000}k - x is invested)")
+    axes.set_ylabel(f"{params.years} year profit minus loan interest")
     axes.yaxis.set_major_formatter(ticker.FormatStrFormatter("$%d"))
     axes.yaxis.grid(True)
 
---
+++ model.py
@@ -0,0 +1,128 @@
+from dataclasses import dataclass
+from typing import Dict
+from typing import Callable
+from typing import List
+
+ExperimentResults = Dict[str, List[int]]
+
+
+@dataclass
+class Model:
+    name: str
+    '''
+    A Sampler accepts as input a number of samples n,
+    and returns returns a time-ordered list of n monthly percent changes of the S&P 500.
+
+    E.g.,
+
+    >>> model.sampler(5)
+    [0.03, -0.02, 0.015, -0.07, 0.1]
+    '''
+    sampler: Callable[[int], List[float]]
+
+
+@dataclass
+class ExperimentParameters:
+    # How many years to simulate, i.e., the length of the loan
+    years: int
+
+    # The amount of cash on hand to be split between
+    # a down payment and investment
+    on_hand_usd: int
+
+    house_price_usd: int
+    mortgage_rate: float
+
+
+def compute_loan_interest(loan_amt, yearly_rate, years):
+    return loan_amt * (1 + yearly_rate / 12)**(12 * years) - loan_amt
+
+
+def simulate_one(sampler, years, capital):
+    num_months = years * 12
+    starting_capital = capital
+    samples = sampler(num_months)
+
+    for monthly_pct_change in samples:
+        capital *= (1 + monthly_pct_change / 100)
+        if capital <= 0:
+            return starting_capital
+
+    return capital - starting_capital
+
+
+def simulate_investment(sampler, years, capital, num_runs=1000):
+    results = []
+    for i in range(num_runs):
+        results.append(simulate_one(sampler, years, capital))
+    return results
+
+
+def run_experiment(model: Model,
+                   params: ExperimentParameters) -> ExperimentResults:
+    minimum_down_payment = int(0.2 * params.house_price_usd)
+    down_payments = []
+    dp = minimum_down_payment
+    while dp < min(params.house_price_usd * 0.8, params.on_hand_usd):
+        down_payments.append(dp)
+        dp += 50000
+
+    loan_interests = dict(
+        (dp,
+         compute_loan_interest(params.house_price_usd -
+                               dp, params.mortgage_rate, params.years))
+        for dp in down_payments)
+    capital_values = dict(
+        (dp, params.on_hand_usd - dp) for dp in down_payments)
+
+    results_dict = dict()
+    for dp, capital in progressBar(capital_values.items(), prefix=model.name):
+        results = simulate_investment(model.sampler,
+                                      capital=capital,
+                                      years=params.years)
+        profits_minus_loan_interest = [
+            profit - loan_interests[dp] for profit in results
+        ]
+        results_dict["{:d}k".format(dp // 1000)] = profits_minus_loan_interest
+
+    return results_dict
+
+
+def progressBar(iterable,
+                prefix='',
+                suffix='',
+                decimals=1,
+                length=50,
+                fill='█',
+                printEnd="\r"):
+    """
+    Copied from https://stackoverflow.com/a/34325723/438830
+
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+
+    Yields a generator that passes through the items from `iterable`
+    with a progress bar printed as a side effect.
+    """
+    total = len(iterable)
+
+    def printProgressBar(iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(
+            100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+
+    printProgressBar(0)
+    for i, item in enumerate(iterable):
+        yield item
+        printProgressBar(i + 1)
+    print()
